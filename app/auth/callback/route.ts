c97e7fd791853ffbfeed359d0e717f7556395c6e
Old <225056366+roy-henderson@users.noreply.github.com>
2025-12-28 20:47:21 -0500

    Add app/auth/callback/route.ts | Full Build | Henderson Standard

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,14 @@
+import { createServerClient } from "@/lib/db/supabase";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+
+  if (code) {
+    const supabase = createServerClient();
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  return NextResponse.redirect(new URL("/dashboard", request.url));
+}
